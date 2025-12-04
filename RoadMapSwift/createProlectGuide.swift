2aede1e7df6323989058009ebefc24ce3f4dc17c
Valery Ponomareva <vale.ponick@gmail.com>
2025-12-04 11:13:48 +0700

    feat: add createProjectGuide.swift

---
+++ RoadMapSwift/createProlectGuide.swift
@@ -0,0 +1,54 @@
+//
+//  createProlectGuide.swift
+//  RoadMapSwift
+//
+//  Created by Валерия Пономарева on 04.12.2025.
+//
+
+import Foundation
+
+/* Задача 1. 'Описание ноутбука': объяви 5 let / var разных типов, характеризующих мой ноутбук. Для каждого свойства укажи тип данных, используя type annotation. Подумайте, какие характеристики д.б. let, а какие - var." */
+    
+/*
+ 📝 Correct steps in Xcode without double nesting:
+ 1.    open Xcode → File → New → Project.
+ 2.    choose sample (App → macOS or iOS).
+ 3.    enter name project (SwiftStudyHub).
+ 4.    ✅ “Create Git repository on my Mac”.
+ 5.    in field Location choose ONLY shared folder:  /Users/твоё_имя/Developer 
+ 6. DON'T GO INSIDE SwiftStudyHub  — Xcode create folder itself.
+ 7.    ✅ Create.
+В итоге структура получится такая:
+ Developer/
+ └── SwiftStudyHub/
+     ├── SwiftStudyHub.xcodeproj
+     ├── SwiftStudyHub/
+     ├── README.md
+     └── .git/
+*/
+
+/* Основные для заметок/шпаргалок
+ 1. ✅ — галочка, успех, проверено/готово
+ 2. ❌ — крестик, ошибка
+ 3. ⚠️ — предупреждение
+ 4. ℹ️ — информация
+ 5. 💡 — идея, совет
+ 6. 📝 — заметка
+ 7. 📖 — открытая книга, руководство
+ 8. ✏️ — карандаш, редактирование
+ 9. 🧑‍💻 — разработчик, программист
+ 10. 🔧 — настройка, инструмент
+ 11. 🚀 — запуск, быстрое действие
+ 12. 🔥 — важное, что требует внимания
+ 13. ❓ — вопрос/непонятно
+ 14. ❗ — срочно/важно
+ 15. ⏳ — ожидание/процесс
+ 16. 🗑️ — удалить
+ 17. 🔄 — обновление/цикл
+ 18. 🆕✨ — новое
+ 19. 💻 — компьютер
+ 20. 🔍 — поиск/debug
+ 21. 📱 — iOS/macOS
+*/
+  
+// Cmd + Shift + Y — запуск проекта! 
